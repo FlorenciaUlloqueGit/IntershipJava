3b8787a7be7bddd1998a05dbdc2f61ed0db9c930
111796 Ulloque Florencia 2w1 <81584057+FlorenciaUlloqueGit@users.noreply.github.com>
2022-01-17 12:17:51 -0300

    Update IdentifyMyParts.java
    updated the comment

--- IntershipExercises/src/Tarea1/IdentifyMyParts.java
+++ IntershipExercises/src/Tarea1/IdentifyMyParts.java
@@ -4,7 +4,7 @@ public class IdentifyMyParts {
     public static int x = 7;
 
     public int y = 3;
-//I'm vreating a comment to see how git show the changes
+//I'm creating a comment to see how git show the changes
     
 
 
